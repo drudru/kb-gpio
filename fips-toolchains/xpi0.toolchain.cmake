ac8b01405bfe351641e8fe3a815443ed9e111cae
Dru Nelson <drudru@gmail.com>
2018-02-13 16:23:57 -0800

    Make it work on linux box

--- fips-toolchains/xpi0.toolchain.cmake
+++ fips-toolchains/xpi0.toolchain.cmake
@@ -2,14 +2,14 @@ SET(CMAKE_SYSTEM_NAME Linux)
 
 # specify the cross compiler
 SET(CMAKE_C_COMPILER
-    /Volumes/crosstools/x-tools/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-gcc)
+    /home/dru/Projs/crosstool-ng/x-tools/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-gcc)
 
 SET(CMAKE_CXX_COMPILER
-    /Volumes/crosstools/x-tools/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-g++)
+    /home/dru/Projs/crosstool-ng/x-tools/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-g++)
 
 # where is the target environment
 SET(CMAKE_FIND_ROOT_PATH
-    /Volumes/crosstools/x-tools/arm-unknown-linux-gnueabihf)
+    /home/dru/Projs/crosstool-ng/x-tools/arm-unknown-linux-gnueabihf)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
